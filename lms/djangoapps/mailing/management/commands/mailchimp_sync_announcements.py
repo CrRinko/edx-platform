3c0da41b1fb847dc06d35a1a9dcf83cb113639d3
Sarina Canelake <sarina@edx.org>
2015-04-14 13:01:49 -0400

    Docstrings for mailchimp scripts

--- lms/djangoapps/mailing/management/commands/mailchimp_sync_announcements.py
+++ lms/djangoapps/mailing/management/commands/mailchimp_sync_announcements.py
@@ -31,6 +31,7 @@ class Command(BaseCommand):
     )
 
     def parse_options(self, options):
+        """Parses `options` of the command."""
         if not options['key']:
             raise CommandError('missing key')
 
